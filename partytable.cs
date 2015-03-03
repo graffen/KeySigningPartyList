fecbd9152e7ddd8c1e03effe0d678e6c9fd80936
Jesper Hess Nielsen <jesper@graffen.dk>
2015-03-03 08:49:50 +0100

    Refactor code a bit

--- partytable.cs
+++ partytable.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 using System.Diagnostics;
 using System.Net;
 
@@ -14,8 +15,61 @@ namespace KeySigningPartyList
                 return;
             }
 
-            string keyInfoStr = GetKeyList(args[0]);
+            Console.WriteLine(GenerateHtmlHeader());
 
+            string keyInfo = GetKeyList(args[0]);
+            ParseKeyList(keyInfo);
+            
+            Console.WriteLine(GenerateHtmlFooter());
+        }
+
+        public static string GetKeyType(string type)
+        {
+            switch(type) {
+                case "1":
+                    return "RSA";
+                case "17":
+                    return "DSA";
+                case "20":
+                    return "El Gamal";
+                case "22":
+                    return "ECDSA";
+                default:
+                    return "Unknown";
+            }
+        }
+
+        private static string SplitFingerPrint(string fpr) 
+        {
+            string prettyFpr = "";
+            for(int i = 0; i < fpr.Length; i += 4) 
+            {
+                prettyFpr += fpr.Substring(i, 4) + " ";
+                if(i == 16) 
+                {
+                    prettyFpr += "\n";
+                }
+            }
+            return prettyFpr;
+        }
+
+        private static string GetKeyList(string keyring) 
+        {
+            Process p = new Process();
+            p.StartInfo.UseShellExecute = false; 
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = "gpg2";
+            p.StartInfo.Arguments = "--fingerprint --no-default-keyring --no-options --with-colons --keyring " + keyring;
+            p.Start();
+
+            string keyInfoStr = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+
+            return keyInfoStr;
+        }
+
+        private static void ParseKeyList(string keyInfo) 
+        {
             bool havePubKey = false;
             bool haveUidForPubKey = false;
             bool haveFprForPubKey = false;
@@ -26,24 +80,9 @@ namespace KeySigningPartyList
             string keyLength = "";
             string uid = "";
 
-            Console.WriteLine("<!DOCTYPE html>\n");
-            Console.WriteLine("<html>\n");
-            Console.WriteLine("<meta charset=\"utf-8\" />\n");
-            Console.WriteLine("<head>\n");
-            Console.WriteLine("<style type=\"text/css\">");
-            Console.WriteLine(@"
-body { font-size: 1em; }
-table { border-width: 1px; border-style: solid; border-color: black; border-collapse: collapse; }
-table th { border: 1px solid black; padding: 2px; font-size: 1.2em; }
-table td { border: 1px solid black; padding: 4px; font-size: 1.2em; }
-table td pre { margin: 0; }");
-            Console.WriteLine("</style>");
-            Console.WriteLine("</head>\n");
-            Console.WriteLine("<body>\n");
-            Console.WriteLine("<h1>Keysigning Party</h1>\n");
-            Console.WriteLine("<table><tr><th>#</th><th>Key ID</th><th>Owner</th><th>Fingerprint</th><th>Size</th><th>Type</th><th>Key Info<br />Matches?</th><th>Owner ID<br />Matches?</th></tr>\n");
             int count = 1;
-            foreach (var line in keyInfoStr.Split('\n'))
+
+            foreach (var line in keyInfo.Split('\n'))
             {
                 var parts = line.Split(':');
                 if (!havePubKey && !haveUidForPubKey && !haveFprForPubKey)
@@ -84,55 +123,38 @@ table td pre { margin: 0; }");
                     count++;
                 }
             }
-            Console.WriteLine("</table>\n");
-            Console.WriteLine("<small>List generated: {0}</small>", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
-            Console.WriteLine("</body>\n");
-            Console.WriteLine("</html>\n");
         }
 
-        public static string GetKeyType(string type)
+        private static string GenerateHtmlHeader() 
         {
-            switch(type) {
-                case "1":
-                    return "RSA";
-                case "17":
-                    return "DSA";
-                case "20":
-                    return "El Gamal";
-                case "22":
-                    return "ECDSA";
-                default:
-                    return "Unknown";
-            }
+            var html = new StringBuilder();
+            html.AppendLine("<!DOCTYPE html>\n");
+            html.AppendLine("<html>\n");
+            html.AppendLine("<meta charset=\"utf-8\" />\n");
+            html.AppendLine("<head>\n");
+            html.AppendLine("<style type=\"text/css\">");
+            html.AppendLine(@"
+body { font-size: 1em; }
+table { border-width: 1px; border-style: solid; border-color: black; border-collapse: collapse; }
+table th { border: 1px solid black; padding: 2px; font-size: 1.2em; }
+table td { border: 1px solid black; padding: 4px; font-size: 1.2em; }
+table td pre { margin: 0; }");
+            html.AppendLine("</style>");
+            html.AppendLine("</head>\n");
+            html.AppendLine("<body>\n");
+            html.AppendLine("<h1>Keysigning Party</h1>\n");
+            html.AppendLine("<table><tr><th>#</th><th>Key ID</th><th>Owner</th><th>Fingerprint</th><th>Size</th><th>Type</th><th>Key Info<br />Matches?</th><th>Owner ID<br />Matches?</th></tr>\n");
+            return html.ToString();
         }
 
-        private static string SplitFingerPrint(string fpr) 
+        private static string GenerateHtmlFooter() 
         {
-            string prettyFpr = "";
-            for(int i = 0; i < fpr.Length; i += 4) 
-            {
-                prettyFpr += fpr.Substring(i, 4) + " ";
-                if(i == 16) 
-                {
-                    prettyFpr += "\n";
-                }
-            }
-            return prettyFpr;
-        }
-
-        private static string GetKeyList(string keyring) 
-        {
-            Process p = new Process();
-            p.StartInfo.UseShellExecute = false;
-            p.StartInfo.RedirectStandardOutput = true;
-            p.StartInfo.FileName = "gpg2";
-            p.StartInfo.Arguments = "--fingerprint --no-default-keyring --no-options --with-colons --keyring " + keyring;
-            p.Start();
-
-            string keyInfoStr = p.StandardOutput.ReadToEnd();
-            p.WaitForExit();
-
-            return keyInfoStr;
+            var html = new StringBuilder();
+            html.AppendLine("</table>\n");
+            html.AppendLine(String.Format("<small>List generated: {0}</small>", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")));
+            html.AppendLine("</body>\n");
+            html.AppendLine("</html>\n");
+            return html.ToString();
         }
     }
 }
